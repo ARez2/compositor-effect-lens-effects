1f6892cc98107a9ce840e20e5119007639a21d86
ARez <75789249+ARez2@users.noreply.github.com>
2026-06-21 21:43:16 +0200

    Update world_environment.gd with more warnings

--- addons/lens_effects/world_environment.gd
+++ addons/lens_effects/world_environment.gd
@@ -3,7 +3,11 @@ extends WorldEnvironment
 
 ## Attach this script to your WorldEnvironment
 
-@export var sun: DirectionalLight3D
+@export var sun: DirectionalLight3D:
+	set(v):
+		if v != sun:
+			sun = v
+			update_configuration_warnings()
 var camera: Camera3D
 
 func _ready() -> void:
@@ -11,12 +15,24 @@ func _ready() -> void:
 		camera = EditorInterface.get_editor_viewport_3d().get_camera_3d()
 	else:
 		camera = get_viewport().get_camera_3d()
+	if !camera:
+		push_error("No camera has been found in the scene. You need at least 1 camera in order for the lens effect to work!")
 
 func _process(_delta: float) -> void:
-	if !sun:
+	if !sun or !camera:
 		return
 	
 	var dir_dot := (-camera.global_transform.basis.z).normalized().dot(sun.global_transform.basis.z.normalized())
 	compositor.compositor_effects[0].sun_position = camera.unproject_position(sun.global_transform.basis.z * maxf(camera.near, 1.0) + camera.global_position)
 	
 	compositor.compositor_effects[0].sun_dir_sign = dir_dot
+
+
+func _get_configuration_warnings():
+	var warnings = []
+
+	if !sun:
+		warnings.append("Please select a DirectionalLight3D to use as reference!")
+
+	# Returning an empty array means "no warning".
+	return warnings
